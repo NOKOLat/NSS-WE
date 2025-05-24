01a17e10c2bd4bf0e43f3d489672eb7923769800
yumi-0000 <s247177@st.go.tuat.ac.jp>
2025-05-24 14:38:46 +0900

    ファイル名変更

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import Ippann from './Pages/Ippann.tsx';
-import Marcop from './Pages/Marcop.tsx';
+import Ippann from './Pages/Plane.tsx';
+import Marcop from './Pages/Marti_copter.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
