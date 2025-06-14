f9c23a859272b26bd60879c77e639b02abd5a91a
yumi-0000 <s247177@st.go.tuat.ac.jp>
2025-06-14 13:42:53 +0900

    ファイル名変更

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Ippann from './Pages/Ippann.tsx';
-import Marcop from './Pages/Marcop.tsx';
+import Marcop from './Pages/Multicopter.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
